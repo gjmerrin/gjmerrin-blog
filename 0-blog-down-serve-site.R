028f439821b95413982a57b76ac26a148a1fd794
Joey Merrin <gjmerrin@gmail.com>
2025-08-26 12:20:33 -0400

    Add update instructions to serve-site script
    Added detailed comments to 0-blog-down-serve-site.R with instructions for updating bio, lab info, contact info, publications, homepage sections, and image handling. Also included tips for using publication scripts and backing up changes before major edits.

--- 0-blog-down-serve-site.R
+++ 0-blog-down-serve-site.R
@@ -1,1 +1,30 @@
+
+# Most Common Updates:
+#   To Update Your Bio:
+#   Edit: content/authors/admin/_index.md
+# 
+# To Update Lab Info:
+#   Edit: content/lab/_index.md
+# 
+# To Update Contact Info:
+#   Edit: config/_default/params.toml
+# 
+# To Add/Edit Publications:
+#   Create or edit files in: content/publication/[paper-name]/index.md
+# 
+# To Update Homepage Sections:
+#   Edit files in: content/home/
+#   
+# Pro Tips:
+#   After editing: The site will auto-reload in your browser (thanks to blogdown::serve_site())
+# 
+# Publication shortcuts: Use the Python scripts we created (create_complete_accurate_publications.py) if you need to batch-add more publications
+# 
+# Images: Place in static/ folder and reference as /image.jpg
+# 
+# Backup: Always commit changes to Git before major edits
+
+
+# Run to update changes to site
+
 blogdown::serve_site()
